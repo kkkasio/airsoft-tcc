264ebe17e03a6fd94a3b37424315a024adc44e43
Kasio Eduardo <kasioeduardo13@gmail.com>
2021-02-01 18:04:46 -0300

    dashboard da liga

---
+++ resources/views/league/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('league.layouts.base')
+
+@section('title', '- Dashboard')
+
+@section('content')
+
+<div class="content">
+    <div class="container-xl">
+        <p>Dashboard da liga!</p>
+    </div>
+</div>
+@endsection
