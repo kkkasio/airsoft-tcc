2a6f270b82dcd43641ed99a69863252aed0a8ff1
Kasio Eduardo <kasioeduardo13@gmail.com>
2021-03-04 16:19:08 -0300

    404 Error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('errors::illustrated-layout')
+
+@section('title', __('SQUAD - Página não encontrada'))
+@section('code', '404')
+@section('message', __('Página não encontrada'))
+
+@section('image')
+<div class="img-box"></div>
+    
+@endsection
